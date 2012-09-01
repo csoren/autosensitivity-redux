d19ef375196564534fee1066d485e917d6d4cf8a
Carsten Elton Sorensen <cso@rift.dk>
2012-09-01 12:39:13 +0200

    Bumped version number

--- AutoSensitivity/Properties/AssemblyInfo.cs
+++ AutoSensitivity/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AutoSensitivity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Igor Kulman")]
+[assembly: AssemblyCompany("Carsten Elton Sørensen")]
 [assembly: AssemblyProduct("AutoSensitivity")]
-[assembly: AssemblyCopyright("Copyright © Igor Kulman 2011")]
+[assembly: AssemblyCopyright("Copyright © Carsten Elton Sørensen 2012, Igor Kulman 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
